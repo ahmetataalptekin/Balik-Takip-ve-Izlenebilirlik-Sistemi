b7149248610f116c3a3884a3423ddf54baaea16d
Mehmet Eymen Eskici <mehmeteymeneskici3@gmail.com>
2025-12-06 19:00:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ function clearAll()
 	let infoTable = document.querySelector('#data');
 	infoTable.innerHTML = '';
 }
-function open_link()
-{
-	const target_id = document.querySelector('#fish_id_input').value;
-	document.querySelector('#id_search').onclick = function ()
-	{
-		window.location.href = "https://balik.yeminlirobot.com/?kayit_id=" + target_id;
-		console.log(target_id);
-	}
-}
+
+document.querySelector('#id_search').addEventListener('click', () => {
+    const target_id = document.querySelector('#fish_id_input').value.trim();
+
+    console.log("Target ID:", target_id);
+
+    if (target_id === "") {
+        alert("Lütfen bir ID girin.");
+        return;
+    }
+
+    window.location.href = "https://balik.yeminlirobot.com/?kayit_id=" + target_id;
+});
