9ba84122b6dc980cf8a727d7e9f906ac4a617fad
Mehmet Eymen Eskici <mehmeteymeneskici3@gmail.com>
2025-12-06 19:25:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ function goToFish() {
     window.location.href = "https://balik.yeminlirobot.com/?kayit_id=" + target_id;
 }
 
+// Butona tıklayınca
 document.querySelector('#id_search').addEventListener('click', goToFish);
 
+// Enter'a basınca
 document.querySelector('#fish_id_input').addEventListener('keydown', (event) => {
     if (event.key === "Enter") {
+        event.preventDefault(); // form varsa sayfa yenilenmesini engeller
         goToFish();
     }
 });
