70f075fef77aeeab141a7a562414794cb3f9bbdb
Mehmet Eymen Eskici <mehmeteymeneskici3@gmail.com>
2025-12-06 18:28:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ error_reporting(E_ALL);
 
 // config.php dosyasını dahil et
 include 'config.php';
+$target_id = $_GET['kayit_id'] ?? 0;
 
 // Veritabanından veri çekmek için SQL sorgusunu yaz
-$sql = "SELECT kayit_id, cinsi, balik_cikis_yontemi FROM balik";
+$sql = "SELECT kayit_id, cinsi, balik_cikis_yontemi, tutuldugu_bolge, sudan_cikis_tarihi, cikaran_gemi_id,
+cikaran_gemi, limana_cikis_tarihi, cikarildigi_liman, tedarikci_id, tedarikci_adi FROM balik WHERE kayit_id = $target_id";
 $result = $conn->query($sql);
 
 // HTML başlık ve stil kısmı
@@ -16,46 +18,84 @@ $result = $conn->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="tr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Veritabanından Veri Çekme</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href = "./style.css" rel = "stylesheet">
 </head>
+
 <body>
+    <div id = "container">
+        <h1 id = "main-title">
+        BALIK TAKIP VE IZLENEBILIRLIK SISTEMI
+        </h1>
+        <h2>
+        Baliklari denizden ciktigi andan itibaren izlenebilirligini saglayan sistem.
+        </h2>
+        <h3>
+        Alacaginiz baligin oldugu kutudaki QR kodu okutun ve asagidaki tabloya bilgileriniz gelecek.
+        Alternatif olarak baligin ID numarasini asagidaki butona tiklayarak da arama yapabilirsiniz.
+        </h3>
+        <input type="text" id="fish_id_input" placeholder="Balık ID'sini Girin"/>
+        <button id="id_search" onclick="location.href='https://balik.yeminlirobot.com/?kayit_id=' + target_id">ID İle Balık Ara</button>
+        
+        <?php if ($target_id != 0): ?>
 
-<div class="container mt-5">
-    <h2>Veritabanından Gelen Bilgi</h2>
-    
-    <!-- Verileri listele -->
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Cinsi</th>
-                <th>Baligin Cikis Yöntemi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            if ($result->num_rows > 0) {
-                // Verileri döngüyle listele
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>
-                            <td>" . $row["kayit_id"] . "</td>
-                            <td>" . $row["cinsi"] . "</td>
-                            <td>" . $row["balik_cikis_yontemi"] . "</td>
-                          </tr>";
+        <h2>Veritabanından Gelen Bilgi</h2>
+        <!-- Verileri listele -->
+        <table class="fish-table">
+            <thead>
+                <?php if ($result->num_rows > 0): ?>
+                <tr>
+                    <th>ID</th>
+                    <th>Cinsi</th>
+                    <th>Balığın Çıkış Yöntemi</th>
+                    <th>Tutulduğu Bölge</th>
+                    <th>Sudan Çıkış Tarihi</th>
+                    <th>Çıkaran Gemi ID</th>
+                    <th>Çıkaran Gemi</th>
+                    <th>Limana Çıkış Tarihi</th>
+                    <th>Çıkarıldığı Liman</th>
+                    <th>Tedarikçi ID</th>
+                    <th>Tedarikçi Adı</th>
+                </tr>
+                <?php endif; ?>
+            </thead>
+            <tbody>
+                <?php
+                if ($result->num_rows > 0)
+                {
+                    // Verileri döngüyle listele
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>
+                                <td>" . $row["kayit_id"] . "</td>
+                                <td>" . $row["cinsi"] . "</td>
+                                <td>" . $row["balik_cikis_yontemi"] . "</td>
+                                <td>" . $row["tutuldugu_bolge"] . "</td>
+                                <td>" . $row["sudan_cikis_tarihi"] . "</td>
+                                <td>" . $row["cikaran_gemi_id"] . "</td>
+                                <td>" . $row["cikaran_gemi"] . "</td>
+                                <td>" . $row["limana_cikis_tarihi"] . "</td>
+                                <td>" . $row["cikarildigi_liman"] . "</td>
+                                <td>" . $row["tedarikci_id"] . "</td>
+                                <td>" . $row["tedarikci_adi"] . "</td>
+                            </tr>";
+                    }
                 }
-            } else {
-                echo "<tr><td colspan='3'>Kayıt bulunamadı</td></tr>";
-            }
-            ?>
-        </tbody>
-    </table>
-</div>
-
+                else
+                {
+                    echo "<tr><td colspan='3'>Kayıt bulunamadı</td></tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+        <?php endif; ?>
+    </div>
+    <script src = "./index.js"></script>
 </body>
+
 </html>
 
 <?php
